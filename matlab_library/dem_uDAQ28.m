59d22045a310003a9947f3f272bd32be98c11dcc
Jan Sefcik <janci.sefcik@gmail.com>
2021-03-18 18:42:10 +0100

    Small fixes in demo functions.

--- matlab_library/dem_uDAQ28.m
+++ matlab_library/dem_uDAQ28.m
@@ -1,4 +1,4 @@
-% Basic functionality in MATLAb for uDAQ28/LT Thermal Plant
+% Basic way of sending/receiving data in MATLAB for uDAQ28/LT Thermal Plant
 % Copyright (c) 2020 Jan Sefcik
 
 udaq_device = serial('/dev/ttyUSB0');  % Set serial port to /dev/ttyUSB0.
@@ -7,13 +7,15 @@ udaq_device.Terminator = 'LF';  % Set terminator to LF.
 fopen(udaq_device);  % Open serial port for communication.
 
 % Execute external script to set serialport baud rate.
-system('~/Documents/uDAQ28LT/baudrate/set_baudrate /dev/ttyUSB0 256000');
+system('./baudrate/set_baudrate /dev/ttyUSB0 256000');
 
+% Write data to serial device and read values from device.
 fprintf(udaq_device,'S255,255,255');
 out = fscanf(udaq_device)
 
 pause(5);
 
+% Write 'turn-off' data to serial device.
 fprintf(udaq_device,'S128,128,128');
 out = fscanf(udaq_device)
 
--- matlab_library/dem_uDAQ28_functions.m
+++ matlab_library/dem_uDAQ28_functions.m
@@ -1,8 +1,10 @@
 % Help functions with examples to work with uDAQ28/LT Thermal Plant.
 % Copyright (c) 2020 Jan Sefcik
 
-dev1 = set_udaq(256000);
-a = write_udaq(dev1,250,250,250)
+dev1 = set_udaq('/dev/ttyUSB0', 256000);
+a = write_udaq(dev1,255,255,255)
+pause(5);
+a1 = write_udaq(dev1,255,255,255)
 pause(5);
 b = write_udaq(dev1,100,100,100)
 pause(5);
@@ -10,14 +12,14 @@ c = write_udaq(dev1,0,0,0)
 delete_udaq(dev1)
 
 % Function to open serial port and set baud rate. 
-function udaq_device = set_udaq(baudrate)
-    udaq_device = serial('/dev/ttyUSB0');  % Set serial port to /dev/ttyUSB0.
+function udaq_device = set_udaq(port, baudrate)
+    udaq_device = serial(port);  % Set serial port to e.g. /dev/ttyUSB0.
     udaq_device.Terminator = 'LF';  % Set terminator to LF.
 
     fopen(udaq_device);  % Open serial port for communication.
 
     % Execute external script to set serialport baud rate.
-    command = strcat('~/uDAQ28LT/baudrate/set_baudrate /dev/ttyUSB0 ', sprintf(' %d',baudrate)); 
+    command = strcat('./baudrate/set_baudrate ', sprintf(' %s %d',port, baudrate)); 
     system(command);
 end
 
